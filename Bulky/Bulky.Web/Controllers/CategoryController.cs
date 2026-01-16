a1b36acaedd50f0637e53bc7cd6a7a20ee8714dd
Fırat Alçın <firatalcin04@gmail.com>
2026-01-16 23:55:40 +0300

    Category List

--- Bulky/Bulky.Web/Controllers/CategoryController.cs
+++ Bulky/Bulky.Web/Controllers/CategoryController.cs
@@ -1,12 +1,22 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Bulky.Web.Data;
+using Bulky.Web.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Bulky.Web.Controllers
 {
     public class CategoryController : Controller
     {
+        private readonly ApplicationDbContext _context;
+
+        public CategoryController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
         public IActionResult Index()
         {
-            return View();
+            List<Category> categoryList = _context.Categories.ToList();
+            return View(categoryList);
         }
     }
 }
