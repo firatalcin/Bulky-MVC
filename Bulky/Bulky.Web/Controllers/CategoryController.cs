d4636a2f8d1803e56bc66a05e7dec95adf6ccd19
Fırat Alçın <firatalcin04@gmail.com>
2026-01-27 00:12:27 +0300

    Edit Category

--- Bulky/Bulky.Web/Controllers/CategoryController.cs
+++ Bulky/Bulky.Web/Controllers/CategoryController.cs
@@ -42,5 +42,30 @@ namespace Bulky.Web.Controllers
 
             return View(category);
         }
+
+        public IActionResult Edit(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            Category category = _context.Categories.Find(id);
+            if (category == null)
+                return NotFound();
+            
+            return View(category);
+        }
+        
+        [HttpPost]
+        public IActionResult Edit(Category category)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Categories.Update(category);
+                _context.SaveChanges();
+                return RedirectToAction("Index", "Category");
+            }
+            return View(category);
+        }
     }
 }
