5164e5eb5245a52964b5a59a6e1c95c643fad6a0
Fırat Alçın <firatalcin04@gmail.com>
2026-02-02 23:52:43 +0300

    Create Razor Page

---
+++ Bulky/Bulky.WebRazor_Temp/Pages/Categories/Create.cshtml.cs
@@ -0,0 +1,29 @@
+using Bulky.WebRazor_Temp.Data;
+using Bulky.WebRazor_Temp.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Bulky.WebRazor_Temp.Pages.Categories
+{
+    [BindProperties]
+    public class CreateModel : PageModel
+    {
+        private readonly ApplicationDbContext _db;        
+        public Category Category { get; set; }
+
+        public CreateModel(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public void OnGet()
+        {
+        }
+
+        public IActionResult OnPost()
+        {
+            _db.Categories.Add(Category);
+            _db.SaveChanges();
+            return RedirectToPage("Index");
+        }
+    }
+}
---
+++ Bulky/Bulky.WebRazor_Temp/Pages/Categories/Index.cshtml.cs
@@ -0,0 +1,23 @@
+using Bulky.WebRazor_Temp.Data;
+using Bulky.WebRazor_Temp.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Bulky.WebRazor_Temp.Pages.Categories
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ApplicationDbContext _db;
+        public List<Category> CategoryList { get; set; }
+
+        public IndexModel(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public void OnGet()
+        {
+            CategoryList = _db.Categories.ToList();
+        }
+    }
+}
