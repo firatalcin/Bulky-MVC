427d1fdc8800d133bfc5a8f30b600c3235a04f32
Fırat Alçın <firatalcin04@gmail.com>
2026-02-02 23:14:00 +0300

    Create Razor Project

---
+++ Bulky/Bulky.WebRazor_Temp/Pages/Index.cshtml.cs
@@ -0,0 +1,13 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Bulky.WebRazor_Temp.Pages
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
---
+++ Bulky/Bulky.WebRazor_Temp/Pages/Privacy.cshtml.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Bulky.WebRazor_Temp.Pages
+{
+    public class PrivacyModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+
+}
